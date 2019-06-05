beeecf6f9f93cb05008cffff1ff093b2cd3e94d0
Donatas Ramanauskas <donatasramanauskas1@gmail.com>
2019-06-05 15:22:13 +0300

    Added post method to FibonacciView

--- fibonaci_api/views.py
+++ fibonaci_api/views.py
@@ -7,3 +7,7 @@ class FibonacciView(APIView):
 
     def get(self, request):
         return Response({"test_field: Velcome to Fibonacci API app", status.HTTP_200_OK})
+
+    def post(self, request):
+        data = request.data.get('number')
+        return Response({"success: you posted {}".format(data), status.HTTP_200_OK})
