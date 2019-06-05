407bda6a3dbd8f8457bf8494f9b454a3fdc79380
Donatas Ramanauskas <donatasramanauskas1@gmail.com>
2019-06-05 17:28:27 +0300

    Added fibonacci_calculator function to FibonacciAPIView

--- fibonaci_api/views.py
+++ fibonaci_api/views.py
@@ -2,6 +2,7 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from .fibonacci_calculator import fibonnaci_calculator
 
 class FibonacciView(APIView):
 
@@ -9,5 +10,6 @@ class FibonacciView(APIView):
         return Response({"test_field: Velcome to Fibonacci API app", status.HTTP_200_OK})
 
     def post(self, request):
-        data = request.data.get('number')
-        return Response({"success: you posted {}".format(data), status.HTTP_200_OK})
+        number = int(request.data.get('number'))
+        result = fibonnaci_calculator(number)
+        return Response(result)
