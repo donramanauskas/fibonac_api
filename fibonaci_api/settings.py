addacf33d56141639945a929b3b3fb88bd84a0af
donramanauskas <donatasramanauskas1@gmail.com>
2019-12-27 15:18:44 +0200

    Changed DB to sqlite

--- fibonaci_api/settings.py
+++ fibonaci_api/settings.py
@@ -79,11 +79,8 @@ WSGI_APPLICATION = 'fibonaci_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
